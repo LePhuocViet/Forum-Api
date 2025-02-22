03f4f2b49a40864e762a84896ecc73dfa60e12e6
LePhuocViet <139203809+LePhuocViet@users.noreply.github.com>
2025-02-22 20:34:01 +0700

    fix post mapper

--- src/main/java/com/project/forum/mapper/PostMapper.java
+++ src/main/java/com/project/forum/mapper/PostMapper.java
@@ -5,8 +5,12 @@ import com.project.forum.dto.responses.user.UserResponse;
 import com.project.forum.enity.Posts;
 import com.project.forum.enity.Users;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
+
+
+    @Mapping(target = "language",source = "language.name")
     PostResponse toPostsResponse(Posts posts);
 }
