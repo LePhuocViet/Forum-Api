ce1db90528c05f56d3f59a62b7acd080bb926e7d
LePhuocViet <139203809+LePhuocViet@users.noreply.github.com>
2025-03-19 10:47:20 +0700

    add token

--- src/main/java/com/project/forum/controller/PostRepostController.java
+++ src/main/java/com/project/forum/controller/PostRepostController.java
@@ -4,6 +4,7 @@ import com.project.forum.dto.responses.post.PostRepostMessage;
 import com.project.forum.dto.responses.post.PostRepostResponse;
 import com.project.forum.exception.ApiResponse;
 import com.project.forum.service.IPostReport;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -17,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class PostRepostController {
 
     IPostReport postReport;
-
+    @SecurityRequirement(name = "BearerAuth")
     @GetMapping()
     ResponseEntity<ApiResponse<Page<PostRepostResponse>>> getAll(@RequestParam(defaultValue = "0") Integer page,
                                                                  @RequestParam(defaultValue = "10") Integer size,
@@ -26,14 +27,14 @@ public class PostRepostController {
                 .data(postReport.getAll(page, size, postId))
                 .build());
     }
-
+    @SecurityRequirement(name = "BearerAuth")
     @DeleteMapping("/{id}")
     ResponseEntity<ApiResponse<PostRepostMessage>> delete(@PathVariable String id) {
         return ResponseEntity.ok(ApiResponse.<PostRepostMessage>builder()
                 .data(postReport.delete(id))
                 .build());
     }
-
+    @SecurityRequirement(name = "BearerAuth")
     @PatchMapping("/{id}")
     ResponseEntity<ApiResponse<PostRepostMessage>> update(@PathVariable String id) {
         return ResponseEntity.ok(ApiResponse.<PostRepostMessage>builder()
